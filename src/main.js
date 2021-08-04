7119a006d35bf1c001e9b5df3fedf494907588cf
FranciscoCharles <charlesdcf123@gmail.com>
2021-08-04 18:06:08 -0300

    otmizando e refatorando codigo

--- src/main.js
+++ src/main.js
@@ -2,22 +2,12 @@ function main() {
 
 	const board = document.querySelectorAll('img')
 	const text_player = document.querySelectorAll('.text-container')
-	const player = 'x'
-	const computer = 'o'
+	const PLAYER = 'x'
+	const COMPUTER = 'o'
 	let is_player_one = true
 
 	text_player[0].style.color = 'white'
 
-	function getWinner() {
-		if (isWinner(computer)) {
-			return computer
-		} else if (isWinner(player)) {
-			return player
-		} else if (isTie()) {
-			return 'tie';
-		}
-		return null
-	}
 	function isTie() {
 		let tie = true
 		for (let row = 0; row < 3; row++) {
@@ -31,6 +21,7 @@ function main() {
 		}
 		return tie
 	}
+
 	function isWinner(player) {
 		if (board[0].dataset.check === player
 			&& board[1].dataset.check === player
@@ -73,9 +64,20 @@ function main() {
 			return true
 		}
 		return false
-
 	}
-	function miniMax(maximize, depth = 0) {
+
+	function getWinner() {
+		if (isWinner(COMPUTER)) {
+			return COMPUTER
+		} else if (isWinner(PLAYER)) {
+			return PLAYER
+		} else if (isTie()) {
+			return 'tie';
+		}
+		return null
+	}
+
+	function miniMax(current_player = PLAYER, depth = 0) {
 		let winner = getWinner()
 
 		if (winner !== null) {
@@ -85,50 +87,51 @@ function main() {
 				tie: 0
 			}[winner]
 		}
-		let best_score = -Infinity
-		if (maximize) {
-			for (let row = 0; row < 3; row++) {
-				for (let column = 0; column < 3; column++) {
-					let element = board[row * 3 + column]
-					if (element.dataset.check === '') {
-						element.dataset.check = computer
 
-						let score = miniMax(false, depth + 1)
-						element.dataset.check = ''
-						best_score = Math.max(best_score, score)
-					}
-				}
-			}
+		let callback = Math.min
+		let best_score = Infinity
+		let next_player = COMPUTER
 
-		} else {
-			best_score = Infinity
-			for (let row = 0; row < 3; row++) {
-				for (let column = 0; column < 3; column++) {
-					let element = board[row * 3 + column]
-					if (element.dataset.check === '') {
-						element.dataset.check = player
+		if (current_player === COMPUTER) {
+			callback = Math.max
+			best_score = -Infinity
+			next_player = PLAYER
+		}
 
-						let score = miniMax(true, depth + 1)
-						element.dataset.check = ''
+		for (let row = 0; row < 3; row++) {
+			for (let column = 0; column < 3; column++) {
+				let element = board[row * 3 + column]
+				if (element.dataset.check === '') {
+					element.dataset.check = current_player
 
-						best_score = Math.min(best_score, score)
-					}
+					let score = miniMax(next_player, depth + 1)
+					element.dataset.check = ''
+					best_score = callback(best_score, score)
 				}
 			}
 		}
 		return best_score
 	}
+
+	function tooglePlayer() {
+		text_player[Number(is_player_one)].style.color = 'white'
+		is_player_one = !is_player_one
+		text_player[Number(is_player_one)].style.color = 'black'
+	}
+
 	function computerMove() {
 		let best_move = null
 		let best_score = -Infinity
+
 		for (let row = 0; row < 3; row++) {
 			for (let column = 0; column < 3; column++) {
+
 				let element = board[row * 3 + column]
 				if (element.dataset.check === '') {
 
-					element.dataset.check = computer
+					element.dataset.check = COMPUTER
 
-					let score = miniMax(false)
+					let score = miniMax()
 					element.dataset.check = ''
 					if (score > best_score) {
 						best_score = score
@@ -139,43 +142,48 @@ function main() {
 		}
 		let element = board[best_move]
 		element.src = 'asserts/o.png'
-		element.dataset.check = computer
+		element.dataset.check = COMPUTER
 		tooglePlayer()
 	}
-	function tooglePlayer() {
-		text_player[Number(is_player_one)].style.color = 'white'
-		is_player_one = !is_player_one
-		text_player[Number(is_player_one)].style.color = 'black'
+
+	function playerVsComputer() {
+		tooglePlayer()
+		let winner = getWinner()
+		if (winner === 'tie' || winner === 'o') {
+			tooglePlayer()
+			alert(
+				winner === 'tie'
+					? 'Congratulations, you tied!'
+					: 'You lost!'
+			)
+			resetGame()
+		} else {
+			computerMove()
+			if (getWinner() === 'o') {
+				alert('You lost!')
+				resetGame()
+			}
+		}
 	}
+
 	function resetGame() {
 		board.forEach(img => {
 			img.dataset.check = ''
 			img.src = 'asserts/empty.png'
 		})
-
 		is_player_one = true
-
 	}
 
 	board.forEach(function addEventClickInImage(img) {
 
 		img.addEventListener('click', function eventClick() {
 			if (img.dataset.check === '') {
-				img.dataset.check = is_player_one ? player : computer
+				img.dataset.check = is_player_one ? PLAYER : COMPUTER
 				img.src = is_player_one ? 'asserts/x.png' : 'asserts/o.png'
-				tooglePlayer()
-				if (getWinner() !== null) {
-					let message = getWinner() === computer
-						? 'Voce perdeu!' :
-						'Parabens, voce empatou!'
-					alert(message)
-					resetGame()
-				} else {
-					computerMove()
-				}
+				playerVsComputer()
 			}
-
 		})
 	})
 }
+
 window.onload = main
